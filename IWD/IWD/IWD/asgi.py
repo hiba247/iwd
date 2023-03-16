3a44c4c110136468e5bd4ffaa81e4722cbd9e592
oussachi <ousstheouss@outlook.com>
2023-03-16 23:45:20 +0100

    project init

---
+++ IWD/IWD/IWD/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for IWD project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IWD.settings')
+
+application = get_asgi_application()
